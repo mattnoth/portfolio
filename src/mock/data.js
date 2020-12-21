4b75c433a7c65bed7d371a36983ff4e6bfa645a8
mattnoth <musicnoth@gmail.com>
2020-12-20 19:17:21 -0600

    simple portfolio

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,64 +2,69 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
+  title: 'Matthew Noth | Developer', // e.g: 'Name | Developer'
   lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
+  description: 'Welcome to my portfolio website', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
   title: '',
-  name: '',
-  subtitle: '',
+  name: 'Matthew Noth',
+  subtitle: `I am a software engineer and web developer.`,
   cta: '',
 };
 
 // ABOUT DATA
 export const aboutData = {
-  img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  img: 'profile-matt.jpeg',
+  paragraphOne:
+    'Go getter software engineer with background in music production, seeking junior front-end developer role in a small to medium sized development house or web based company. An adaptive thinker who particularly enjoys utilizing creative flair to create enjoyable user experiences by using simple solutions to coding problems and building lightweight applications.',
+  paragraphTwo:
+    'I offer a unique mix of organization and imagination, connecting creative skills built through music production with software development, displaying data in both standard and unique ways particular to the user experience at hand.',
+  paragraphThree: `Check out my projects-`,
+  resume: 'https://drive.google.com/file/d/17uXT5w2fRKMtU5DwwOfWLpaBaVg2FDSH/view?usp=sharing', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'simon-project.png',
+    title: 'Simon',
+    info:
+      'Developed a recreation of the classic memory game Simon using vanilla JS / CSS and HTML, which implements an entirely in scope ES6 class constructor.  Created during General Assembly Bootcamp where the prompt was to create one of 6 games. Building the architecture for the timing & movement was the biggest hurdle.',
     info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    url: 'https://mattnoth.github.io/simon/',
+    repo: 'https://github.com/mattnoth/simon', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'Diskshelf-Project.png',
+    title: 'Diskshelf',
+    info:
+      'Ongoing project; a  “video game encyclopedia” with React.js, using a third party API, RAWG, to dynamically fetch data via user input. Users can search a string of test, or filter via a platform, genre, or decade. Clicking on a card generates a game details page, which includes video, pictures and other information about the selection. ',
     info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    url: 'https://diskshelf.herokuapp.com',
+    repo: 'https://github.com/mattnoth/diskshelf', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'botanical.png',
+    title: 'Botanical Babble',
+    info:
+      'A group project for GA SEI, with a backend and frontend, MERN stack. Full CRUD website for discussion surrounding plants.',
     info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    url: 'https://bbfe.herokuapp.com/',
+    repo: 'https://github.com/botanicalbabble/front-end', // if no repo, the button will not show up
   },
 ];
 
 // CONTACT DATA
 export const contactData = {
   cta: '',
-  btn: '',
-  email: '',
+  btn: 'musicnoth@gmail.com',
+  email: 'musicnoth@gmail.com',
 };
 
 // FOOTER DATA
@@ -68,7 +73,7 @@ export const footerData = {
     {
       id: nanoid(),
       name: 'twitter',
-      url: '',
+      url: 'https://twitter.com/moodsmithmusic?lang=en',
     },
     {
       id: nanoid(),
@@ -78,17 +83,17 @@ export const footerData = {
     {
       id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'https://www.linkedin.com/in/mattnoth/',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/mattnoth',
     },
   ],
 };
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
 };
